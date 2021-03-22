e0cc76caa852194d018dc164e1cf6d3b47dd97ce
a16pednieper_clase <a16pednieper@inspedralbes.cat>
2021-03-22 10:40:10 +0100

    cambios_importantes_vue

---
+++ borsa_terball_grup7/src/components/Buscador.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+    <h1>prova Buscador</h1>
+    <b-button @click="pedirDatos" variant="primary">Cerrar</b-button>
+</div>       
+</template>
+
+
+<script>
+import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+Vue.use(VueAxios, axios);
+
+export default{
+      data() {
+          return{
+              resultados: []
+          }
+        
+    },
+    methods: {
+        pedirDatos() {
+            axios.get(`http://labs.iam.cat/~a16pednieper/treball_g7/api_treball.php/records/oferta`).then((response) => {this.resultados = response.data.records; console.log(response) })
+        }
+    }
+}
+</script>
---
+++ borsa_terball_grup7/src/components/Oferta.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>prova</h1>
+</template>
+
+<script>
+
+export default{
+    data() {
+        
+    },
+    methods: {
+        pedirDatos() {
+            axios.get(`http://labs.iam.cat/~a16pednieper/treball_g7/api_treball.php/records/oferta`).then((response) => {this.detailedResults = response.data; console.log(response)})
+        }
+    }
+}
+</script>
