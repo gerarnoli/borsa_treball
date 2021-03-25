781a35de11f4c4a525e3c7764a6986f42e68436f
GunterRL <60893006+GunterRL@users.noreply.github.com>
2021-03-25 09:36:34 +0100

    header

--- borsa_terball_grup7/src/components/Header.vue
+++ borsa_terball_grup7/src/components/Header.vue
@@ -1,14 +1,36 @@
 <template>
+<div id="app">
+  <v-app id="inspire">
+    <v-row justify="left">
+      <v-dialog
+        v-model="dialog"
+        persistent
+        max-width="600px"
+      >
+  <template v-slot:activator="{ on, attrs }">
+    <v-btn
+    color="primary" rounded dark v-bind="attrs"
+    v-on="on"
+    >
+    Iniciar Sessió
+    </v-btn>
+    <v-btn
+    color="primary" rounded dark v-bind="attrs"
+    v-on="on"
+    >
+    Registre
+    </v-btn>
+
+
+
   <v-header padless>
     <v-col class="text-left indigo float-left" cols="12">
-      <v-btn color="primary" rounded>Iniciar Sessió</v-btn>
       <v-divider class="ma-4" vertical style="display: inline"></v-divider>
-      <v-btn color="primary" rounded>Iniciar Sessió</v-btn>
       <b-button href="http://localhost:8000/oferta/new" class="empresa" variant="primary">Ets empresa?</b-button>
       <Register/>
-
     </v-col>  
   </v-header>
+
 </template>
 
 <script>
